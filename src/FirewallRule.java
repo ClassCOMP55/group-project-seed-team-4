ccc805ade467c4b59329ec05c9d9354c5babb987
Darrel Mbanefo <d_mbanefo@u.pacific.edu>
2026-03-16 16:46:10 -0700

    added new constructor FirewallRule

--- src/FirewallRule.java
+++ src/FirewallRule.java
@@ -4,4 +4,11 @@ public class FirewallRule {
 	private int portRange;
 	private double threatThreshold;
 	private Decision decision;
+	
+	public FirewallRule(PacketType packetType, int portRange, double threatThreshold, Decision decision) {
+		this.packetType = packetType;
+		this.portRange = portRange;
+		this.threatThreshold = threatThreshold;
+		this.decision = decision;
+	}
 }
