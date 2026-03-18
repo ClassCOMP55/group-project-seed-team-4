7e79bca37a535646181c8c26cd9d7bb2307e1eae
Darrel Mbanefo <d_mbanefo@u.pacific.edu>
2026-03-18 10:11:33 -0700

    declaration result

--- src/Result.java
+++ src/Result.java
@@ -3,4 +3,10 @@ public class Result {
 	public boolean isCorrect;
 	public Decision decision;
 	public String reason;
+	
+	public Result(boolean isCorrect, Decision decision, String reason) {
+		this.isCorrect = isCorrect;
+		this.decision = decision;
+		this.reason = reason;
+	}
 }
