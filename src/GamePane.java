4cac6f05f3c06dfde3f37a9121a67491ab6e9d26
Darrel Mbanefo <d_mbanefo@u.pacific.edu>
2026-03-19 14:08:02 -0700

    added a constructor for GamePane

--- src/GamePane.java
+++ src/GamePane.java
@@ -1,4 +1,9 @@
+import java.awt.event.MouseEvent;
+import acm.graphics.*;
 
-public class GamePane {
-
+public class GamePane extends GraphicsPane{
+	
+	public GamePane(MainApplication mainScreen) {
+		this.mainScreen = mainScreen;
+	}
 }
