f74e23675c03a8b4adbf8e1c3c5fa22f9e45f6b0
Darrel Mbanefo <d_mbanefo@u.pacific.edu>
2026-03-19 15:48:51 -0700

    added showcontent settings

--- src/SettingsPane.java
+++ src/SettingsPane.java
@@ -6,4 +6,15 @@ public class SettingsPane extends GraphicsPane{
 	public SettingsPane(MainApplication mainScreen) {
 		this.mainScreen = mainScreen;
 	}
+	
+	public void showContent() {
+		GLabel title = new GLabel("Settings", 100, 100);
+		GLabel back = new GLabel("Back", 100, 300);
+		
+		contents.add(title);
+		contents.add(back);
+		
+		mainScreen.add(title);
+		mainScreen.add(back);
+	}
 }
