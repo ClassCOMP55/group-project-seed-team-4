6e68e8a42b1fc8f69ccad8c4fec7d91e57d0c37f
Darrel Mbanefo <d_mbanefo@u.pacific.edu>
2026-03-19 13:20:59 -0700

    added contents and mainScreen

--- src/DifficultyPane.java
+++ src/DifficultyPane.java
@@ -12,5 +12,15 @@ public class DifficultyPane extends GraphicsPane{
 		GLabel medium = new GLabel("Pro", 100, 250);
 		GLabel hard = new GLabel("Hacker", 100, 300);
 		GLabel back = new GLabel("Back", 100, 400);
+		
+		contents.add(easy);
+		contents.add(medium);
+		contents.add(hard);
+		contents.add(back);
+		
+		mainScreen.add(easy);
+		mainScreen.add(medium);
+		mainScreen.add(hard);;
+		mainScreen.add(back);
 	}
 }
