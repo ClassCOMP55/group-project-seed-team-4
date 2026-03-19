b781f5c6a342da66de49ff6a272e7ac90e3e75c9
Darrel Mbanefo <d_mbanefo@u.pacific.edu>
2026-03-19 13:17:02 -0700

    added labels

--- src/DifficultyPane.java
+++ src/DifficultyPane.java
@@ -1,4 +1,16 @@
+import java.awt.event.MouseEvent;
+import acm.graphics.*;
 
-public class DifficultyPane {
+public class DifficultyPane extends GraphicsPane{
+	
+	public DifficultyPane(MainApplication mainScreen) {
+		this.mainScreen = mainScreen;
+	}
 
+	public void showContent() {
+		GLabel easy = new GLabel("Noob", 100, 200);
+		GLabel medium = new GLabel("Pro", 100, 250);
+		GLabel hard = new GLabel("Hacker", 100, 300);
+		GLabel back = new GLabel("Back", 100, 400);
+	}
 }
