ffb77564f7fee05763fe6a03de64432a2aaeb237
Darrel Mbanefo <d_mbanefo@u.pacific.edu>
2026-03-20 09:51:11 -0700

    Added show contents to shop

--- src/ShopPane.java
+++ src/ShopPane.java
@@ -5,5 +5,16 @@ public class ShopPane extends GraphicsPane {
 	public ShopPane(MainApplication mainApplication) {
 		this.mainScreen = mainScreen;
 	}
+	
+	public void showContent() {
+		GLabel title = new GLabel("SHOP", 100, 100);
+		GLabel back = new GLabel("Back", 100, 300);
+		
+		contents.add(title);
+		contents.add(back);
+		
+		mainScreen.add(title);
+		mainScreen.add(back);
+	}
 
 }
