71ab58e2da67864d849215ac2e2c23c13ce7bca6
Darrel Mbanefo <d_mbanefo@u.pacific.edu>
2026-03-26 14:46:41 -0700

    just a placeholder for now

--- src/LevelManager.java
+++ src/LevelManager.java
@@ -1,5 +1,33 @@
 import java.util.ArrayList;
 
 public class LevelManager {
+	private ArrayList<Level> levels;
+    private int currentLevelIndex;
 
+    public LevelManager() {
+        levels = new ArrayList<>();
+        currentLevelIndex = 0;
+
+        levels.add(new Level(1, 100, 1.0)); 
+        levels.add(new Level(2, 200, 1.5)); 
+        levels.add(new Level(3, 300, 2.0)); 
+    }
+
+    public Level getCurrentLevel() {
+        return levels.get(currentLevelIndex);
+    }
+
+    public void advanceLevel() {
+        if (currentLevelIndex < levels.size() - 1) {
+            currentLevelIndex++;
+        }
+    }
+
+    public void resetLevels() {
+        for (Level level : levels) {
+            level.resetLevel();
+        }
+        currentLevelIndex = 0;
+    }
 }
+
