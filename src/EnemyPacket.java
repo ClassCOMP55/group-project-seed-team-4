500a2d7f509396ccabf4f9e5a3586412e66a7598
Darrel Mbanefo <d_mbanefo@u.pacific.edu>
2026-03-26 16:15:12 -0700

    Created constructor

--- src/EnemyPacket.java
+++ src/EnemyPacket.java
@@ -1,9 +1,14 @@
 
 public class EnemyPacket extends Packet{
+	private String enemyType;
+	private int damage;
+	private boolean isSpawned;
 
 	public EnemyPacket(int id, PacketType type, String sourceIp, int port, double threatScore) {
 		super(id, type, sourceIp, port, threatScore);
-		// TODO Auto-generated constructor stub
+        this.enemyType = enemyType;
+        this.damage = damage;
+        this.isSpawned = true;
 	}
 	
 }
