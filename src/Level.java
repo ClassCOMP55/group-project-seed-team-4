1293b373021d87d6b61e8d7eaf6281f5673b75f4
Darrel Mbanefo <d_mbanefo@u.pacific.edu>
2026-03-26 14:15:54 -0700

    added constructors

--- src/Level.java
+++ src/Level.java
@@ -4,4 +4,11 @@ public class Level {
     private int targetScore;
     private double spawnRate; 
     private boolean isComplete;
+    
+    public Level(int levelNumber, int targetScore, double spawnRate) {
+    	this.levelNumber = levelNumber;
+    	this.targetScore = targetScore;
+    	this.spawnRate = spawnRate;
+    	this.isComplete = false;
+    }
 }
