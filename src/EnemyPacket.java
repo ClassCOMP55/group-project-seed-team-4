02bbb498f62c025edf48aeebac1fefa94b7dd6d1
Darrel Mbanefo <d_mbanefo@u.pacific.edu>
2026-03-26 16:04:49 -0700

    new class enemypacket

---
+++ src/EnemyPacket.java
@@ -0,0 +1,9 @@
+
+public class EnemyPacket extends Packet{
+
+	public EnemyPacket(int id, PacketType type, String sourceIp, int port, double threatScore) {
+		super(id, type, sourceIp, port, threatScore);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
