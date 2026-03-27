8de823e564af6ee3d184780f057c2df153a1f298
Darrel Mbanefo <d_mbanefo@u.pacific.edu>
2026-03-27 10:09:38 -0700

    made changes to Judge class

--- src/Judge.java
+++ src/Judge.java
@@ -1,4 +1,14 @@
 
 public class Judge {
-	
+	public Result evaluateDrop(Packet packet, Zone zone, RuleSet ruleSet) {
+		Decision decision = convertZoneToDecision(zone);
+		Result result = new Result(false, decision, "");
+		
+		return null;
+	}
+
+	private Decision convertZoneToDecision(Zone zone) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
