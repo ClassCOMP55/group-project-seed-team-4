0de9e3343e6f9be8c4b5558253a280cd2245c743
Darrel Mbanefo <d_mbanefo@u.pacific.edu>
2026-04-04 15:40:02 -0700

    new class currency manager

---
+++ src/CurrencyManager.java
@@ -0,0 +1,9 @@
+
+public class CurrencyManager {
+	private int secureTokens;
+	private static final double[] CONVERSION_RATES = {0.02, 0.05, 0.08}; //NOOB, PRO, HACKER
+	
+	public CurrencyManager() {
+		this.secureTokens = 0;
+	}
+}
