35b8a9cc99ef8779b7ff83876a27d5d83642f62e
Darrel Mbanefo <d_mbanefo@u.pacific.edu>
2026-04-12 19:11:52 -0700

    editing sound player for music to be stopped

--- src/SoundPlayer.java
+++ src/SoundPlayer.java
@@ -2,17 +2,19 @@ import javax.sound.sampled.*;
 import java.io.InputStream;
 
 public class SoundPlayer {
+	private Clip clip;
     public void playSound(String resourcePath) {
-        try {
+    	try {
+            //stopSound(); // stop previous if any
             InputStream audioSrc = getClass().getResourceAsStream(resourcePath);
+            if (audioSrc == null) return;
             InputStream bufferedIn = new java.io.BufferedInputStream(audioSrc);
             AudioInputStream audioIn = AudioSystem.getAudioInputStream(bufferedIn);
 
-            Clip clip = AudioSystem.getClip();
+            clip = AudioSystem.getClip();
             clip.open(audioIn);
             clip.start();
             clip.loop(Clip.LOOP_CONTINUOUSLY);
-
         } catch (Exception e) {
             e.printStackTrace();
         }
