7b44d8f14a7bc689a706e4474ceed24217910f1f
Darrel Mbanefo <d_mbanefo@u.pacific.edu>
2026-04-14 18:40:50 -0700

    Added save_file to save player money

--- src/CurrencyManager.java
+++ src/CurrencyManager.java
@@ -2,19 +2,23 @@
 public class CurrencyManager {
 	private int secureTokens;
 	private static final double[] CONVERSION_RATES = {0.02, 0.05, 0.08}; //NOOB, PRO, HACKER
+	private static final String SAVE_FILE = "secure_tokens.dat";
 	
 	public CurrencyManager() {
 		this.secureTokens = 0;
+		//load();
 	}
 	
 	public void addTokensFromScore(int score, String difficulty) {
-		double rate = CONVERSION_RATES[0];
-		if (difficulty.equals("PRO")) rate = CONVERSION_RATES[1];
-		else if (difficulty.equals("HACKER")) rate = CONVERSION_RATES[2];
-		
-		int tokensEarned = (int) (score * rate);
-		secureTokens += tokensEarned;
-	}
+        double rate = CONVERSION_RATES[0];
+        if ("PRO".equals(difficulty)) rate = CONVERSION_RATES[1];
+        else if ("HACKER".equals(difficulty)) rate = CONVERSION_RATES[2];
+
+        int tokensEarned = (int) (score * rate);
+        secureTokens += tokensEarned;
+        //save();
+    }
+
 	
 	public boolean spendTokens(int amount) {
 		if (secureTokens >= amount) {
